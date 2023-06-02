60f2435f3a7eecbb3341aaf3902be3e9ef433fcf
1Mill <erik.ekberg@outlook.com>
2023-06-02 11:00:50 -0700

    we added a jose button

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -29,6 +29,7 @@
 			<button @click="()=>addStyle({borderRadius: '100%'})">jmi</button>
 			<button @click="() => addStyle({ borderRadius: '100%', backgroundColor: 'green', transform: 'skew(30deg)'})">Door</button>
 			<button @click="()=>addStyle({background: 'pink'})">mr roll</button>
+			<button @click="()=>addStyle({ transform: 'rotate(20deg)' })">jose</button>
 
 		</section>
 	</main>
